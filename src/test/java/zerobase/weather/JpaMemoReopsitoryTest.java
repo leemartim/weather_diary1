2a7fcf3a0c2598ddebd1beafa3548b6544c43380
DESKTOP-R7ANO7G\lee <nicet.shlee@gmail.com>
2022-10-06 13:18:42 +0900

    jpa DB 연결 및 jpa 연결 테스트

---
+++ src/test/java/zerobase/weather/JpaMemoReopsitoryTest.java
@@ -0,0 +1,45 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JpaMemoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@Transactional
+public class JpaMemoReopsitoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo newMemo = new Memo(10, "this is jpa memo");
+        //when
+        jpaMemoRepository.save(newMemo);
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertTrue(memoList.size() > 0);
+    }
+
+    @Test
+    void findByIdTest() {
+        //given
+        Memo newMemo = new Memo(11, "jpa"); // auto increment때문에 사실 id를 넣는건 의미가 없음
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        System.out.println(memo.getId());
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals(result.get().getText(), "jpa");
+    }
+}
